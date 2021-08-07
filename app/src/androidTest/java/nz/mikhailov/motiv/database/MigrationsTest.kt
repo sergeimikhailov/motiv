a9d14eadc0d83b6186036561b5f7e124fd8959e5
Sergei Mikhailov <sergei@mikhailov.nz>
2021-08-07 23:40:47 +1200

    tidy up

--- app/src/androidTest/java/nz/mikhailov/motiv/database/MigrationsTest.kt
+++ app/src/androidTest/java/nz/mikhailov/motiv/database/MigrationsTest.kt
@@ -1,22 +1,20 @@
-package nz.mikhailov.motiv
+package nz.mikhailov.motiv.database
 
 import androidx.room.testing.MigrationTestHelper
 import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import nz.mikhailov.motiv.database.MIGRATION_1_2
-import nz.mikhailov.motiv.database.MotivRoomDatabase
-import org.junit.Assert.assertArrayEquals
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.time.Instant.now
+import java.time.Instant
 import java.time.ZoneId
 
 const val TEST_DB = "migration-test"
 
 @RunWith(AndroidJUnit4::class)
-class MigrationTest {
+class MigrationsTest {
 
     @get:Rule
     val helper: MigrationTestHelper = MigrationTestHelper(
@@ -26,16 +24,17 @@ class MigrationTest {
     )
 
     @Test
-    fun migrate1to2() {
-        helper.createDatabase(TEST_DB, 1).use {
+    fun migrate1to2() = with(helper) {
+        createDatabase(TEST_DB, 1).use {
             it.execSQL("""
                 INSERT INTO transactions(date, amount) VALUES
                     ('2021-06-12 17:59', 1),
                     ('2021-06-12 18:02', -11)
             """.trimIndent())
         }
-        helper.runMigrationsAndValidate(TEST_DB, 2, true, MIGRATION_1_2).use {
-            val offsetMillis = ZoneId.systemDefault().rules.getOffset(now()).totalSeconds * 1000
+        runMigrationsAndValidate(TEST_DB, 2, true, MIGRATION_1_2).use {
+            val offsetMillis =
+                ZoneId.systemDefault().rules.getOffset(Instant.now()).totalSeconds * 1000
             val result = mutableListOf<Pair<Long, Int>>()
             it.query("SELECT date, amount FROM transactions").use { cursor ->
                 while (cursor.moveToNext()) {
@@ -44,7 +43,7 @@ class MigrationTest {
                     result.add(Pair(date, amount))
                 }
             }
-            assertArrayEquals(
+            Assert.assertArrayEquals(
                 arrayOf(
                     Pair(1623520740000 - offsetMillis, 1),
                     Pair(1623520920000 - offsetMillis, -11)),
