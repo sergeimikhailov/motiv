679edf71d020fab5961ec1af83a0a45b97f96dc4
Sergei Mikhailov <sergei@mikhailov.nz>
2021-08-08 14:57:50 +1200

    switching context for usecases work

--- app/src/main/java/nz/mikhailov/motiv/feature/transactions/business/TransactionsUseCase.kt
+++ app/src/main/java/nz/mikhailov/motiv/feature/transactions/business/TransactionsUseCase.kt
@@ -1,9 +1,9 @@
 package nz.mikhailov.motiv.feature.transactions.business
 
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import nz.mikhailov.motiv.feature.transactions.TransactionsFeature
-import nz.mikhailov.motiv.feature.transactions.business.model.Transaction
 import nz.mikhailov.motiv.feature.transactions.business.model.toBo
 import nz.mikhailov.motiv.feature.transactions.data.TransactionRepository
 import nz.mikhailov.motiv.feature.transactions.data.model.TransactionRecord
@@ -12,11 +12,14 @@ import java.time.Instant
 class TransactionsUseCase(
     private val transactionRepository: TransactionRepository = TransactionRepository(),
 ) : TransactionsFeature {
-    override suspend fun getLatestTransactions(): Flow<List<Transaction>> =
+    
+    override suspend fun getLatestTransactions() = withContext(Dispatchers.IO) {
         transactionRepository.transactionRecords
             .map { records -> records.map(TransactionRecord::toBo) }
+    }
 
-    override suspend fun deposit(amount: Int) =
+    override suspend fun deposit(amount: Int) = withContext(Dispatchers.IO) {
         transactionRepository
             .insert(TransactionRecord(Instant.now(), amount))
+    }
 }
