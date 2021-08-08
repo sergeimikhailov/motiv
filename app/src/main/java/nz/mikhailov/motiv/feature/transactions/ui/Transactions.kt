daecc66ae94b13e791428ff850b2be6c000b958f
Sergei Mikhailov <sergei@mikhailov.nz>
2021-08-08 15:23:57 +1200

    cleaner ui for transaction history

--- app/src/main/java/nz/mikhailov/motiv/feature/transactions/ui/Transactions.kt
+++ app/src/main/java/nz/mikhailov/motiv/feature/transactions/ui/Transactions.kt
@@ -1,5 +1,9 @@
 package nz.mikhailov.motiv.feature.transactions.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
@@ -7,7 +11,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import nz.mikhailov.motiv.feature.transactions.business.model.Transaction
+import nz.mikhailov.motiv.ui.theme.Green
 import nz.mikhailov.motiv.ui.theme.MotivTheme
 
 @Composable
@@ -19,8 +25,29 @@ fun Transactions(
         modifier = modifier,
     ) {
         items(transactions) { transaction ->
-            Text(text = "${transaction.date} - $${transaction.amount}")
+            TransactionItem(
+                transaction = transaction,
+            )
         }
+    }
+}
+
+@Composable
+private fun TransactionItem(
+    modifier: Modifier = Modifier,
+    transaction: Transaction,
+) {
+    Row(
+        modifier = modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(text = "${transaction.date}")
+        Text(
+            text = "$${transaction.amount}",
+            color = Green,
+        )
     }
 }
 
@@ -31,8 +58,8 @@ fun TransactionsPreview() {
         Surface {
             Transactions(
                 transactions = listOf(
-                    Transaction(1, "2021-10-07 19:32"),
-                    Transaction(2, "2021-10-07 19:31"),
+                    Transaction(1, "8 August 2021 13:24"),
+                    Transaction(2, "23 July 2021 19:31"),
                 ),
             )
         }
